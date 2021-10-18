947ab73c1fb339dd6fb8971d11a91149111f95f7
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-10-18 15:49:49 -0300

    Finalizando o desafio

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,15 +30,29 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+
+    const logins = await AsyncStorage.getItem(dataKey);
+    const parsedLogins = logins ? JSON.parse(logins) : [];
+
+    setSearchListData(parsedLogins);
+    setData(parsedLogins);
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    if (searchText) {
+      const result = data.filter(item => {
+        return item.service_name.includes(searchText)
+      });
+
+      setSearchListData(result);
+    }
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    if (text === '') {
+      setSearchListData(data);
+    }
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
